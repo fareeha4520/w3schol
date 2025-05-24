591d98f4b7a7f0d5c2e1cd8043e9fc3541943d0d
Anishali2 <ansihali1221@gmail.com>
2025-05-25 04:42:20 +0500

    images added into freeha website

--- src/components/code-editor.jsx
+++ src/components/code-editor.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Editor = () => {
   return (
     <>
-    <div className='editor-container'>
-      <div className='edit'>
-        <h1>Code Editor</h1></div>
-      <div className='subText'>
-      With our online code editor, you can edit code and view the result in your browser
-    </div>
-    <div className='edit-video'>
-    <img src='User/app/html-css GIF.webp'></img>
+      <div className="editor-container">
+        <div className="edit">
+          <h1>Code Editor</h1>
+        </div>
+        <div className="subText">
+          With our online code editor, you can edit code and view the result in
+          your browser
+        </div>
+        <div className="edit-video">
+          <img src="/codeeditor.webp"></img>
+        </div>
+
+        <div className="editor-button">Try Frontend Ediotor (HTML/CSS/JS)</div>
+        <div className="editor-button" id="color">
+          Try Backend Ediotor (Python/PHP/Java/C...)
+        </div>
       </div>
-    
-      <div className='editor-button'>Try Frontend Ediotor (HTML/CSS/JS)</div>
-      <div className='editor-button' id='color'>Try Backend Ediotor (Python/PHP/Java/C...)</div>
-      
-
-   </div>
-
-
-    
     </>
   );
-}
+};
 
 export default Editor;
+
--- src/components/spaces.jsx
+++ src/components/spaces.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Spaces = () => {
   return (
     <>
-        <div className='spaces-container'>
-      <div className='space'>
-        <h1>W3Schools Spaces</h1></div>
-      <div className='space-text'>
-       If you want to create your own website,check out <span className='color-text'> w3school spaces </span>.
-       <br></br>
-       It is free to use,and does not require any setup:
-    </div>
-    <div className='spacing-video'>
-        <video width="850" height="400" autoPlay  loop controls>
-        <source src="https://www.w3schools.com/spaces/dynamicspaces.webp"/>
-      </video>
+      <div className="spaces-container">
+        <div className="space">
+          <h1>W3Schools Spaces</h1>
+        </div>
+        <div className="space-text">
+          If you want to create your own website,check out{" "}
+          <span className="color-text"> w3school spaces </span>.<br></br>
+          It is free to use,and does not require any setup:
+        </div>
+        <div className="spacing-video">
+          {/* freeha ya video nahi ha app usa rightclick karka daikh leya karo jo video chal rahi the wo basically gif tha or gif <img/> ka tag ma lagta ha naa ka <video/> */}
+          {/* or second cheez ya ka app na image ya video public ka folder ma save karna ha or usa aasa call karna ha jasa mana ider nicha call kar deya ha */}
+          <img src="/dynamicspaces.webp"></img>
+        </div>
+        <div className="learning-btn">
+          <div className="spacing-button">Learn More</div>
+        </div>
       </div>
-    <div className='learning-btn'>
-    <div className='spacing-button'>Learn More</div>
-</div>
-
-    </div>
     </>
   );
-}
+};
 
 export default Spaces;
+
